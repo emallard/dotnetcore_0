5784f3ed7570b7b052d808c16f307546dadaa407
Etienne <etienne.mallard@gmail.com>
2018-02-18 21:43:12 +0100

    d

--- ci/tasks/build.sh
+++ ci/tasks/build.sh
@@ -8,10 +8,10 @@ ls -l ../
 
 echo "Starting build ..."
 
-dotnet restore
-dotnet publish -c Release -o out
+#dotnet restore
+#dotnet publish -c Release -o out
 
 #echo "copying files to ../build-output"
 #cp manifest.yml ../build-output
 mkdir -p ../out
-cp -R ./out/* ../out
+#cp -R ./out/* ../out
