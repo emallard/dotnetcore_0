7478937ec3b583c128d42d28866eba1611350477
Etienne <etienne.mallard@gmail.com>
2018-02-18 18:41:52 +0100

    CI

---
+++ ci/tasks/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e -u -x
+
+cd sources
+
+echo I am in `pwd`
+ls -l ../
+
+echo "Starting build ..."
+
+dotnet restore
+dotnet publish -c Release -o out
+
+#echo "copying files to ../build-output"
+#cp manifest.yml ../build-output
+mkdir -p ../out
+cp -R ./out/* ../out
